4229ab46a8211fe80a35c39585d235c3d063362d
HumanHiHuBe <76218894+HumanHiHuBe@users.noreply.github.com>
2022-09-25 14:27:31 +0530

    Update djangoProject&FirstAppCreation&OpenProjectInVSCodeAutomator.py

--- djangoProject&FirstAppCreation&OpenProjectInVSCodeAutomator.py
+++ djangoProject&FirstAppCreation&OpenProjectInVSCodeAutomator.py
@@ -1,7 +1,7 @@
 """
-This python module allows you to start your project and first app also open the project level folder in VS code in one go.
-All you have To Do is Place This File in Your Favourite Folder Where you create All your Django Projects and just
-'Enter your Project & App Name' when prompted to do so.
+This python file allows you to start your project and first app also open the project level folder in VS code in one go.
+All you have To Do is Place This File in Your Favourite Folder Where you create All your Django Projects
+and just 'Enter your Project & App Name' when prompted to do so.
 Please enter the name of your Project & App as per rule of Python Identifiers.
 Please refer to 'https://docs.python.org/3/reference/lexical_analysis.html#identifiers' for official information related to python Identifiers.
 
